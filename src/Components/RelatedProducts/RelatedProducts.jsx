205234ba7c05d07158fc494452c23db6d82ac6d0
mouMon25 <moumon1925@gmail.com>
2025-08-07 00:43:49 +0530

    RelatedProducts.jsx

--- src/Components/RelatedProducts/RelatedProducts.jsx
+++ src/Components/RelatedProducts/RelatedProducts.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import './RelatedProducts.css'
 import Item from '../Item/Item'
-import { backend_url } from "../../App/App.jsx";
+import { backend_url} from '../../App.jsx';
 
 const RelatedProducts = ({category,id}) => {
 
