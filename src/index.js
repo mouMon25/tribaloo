1715ae103accd51dd400858ea32aa857cbab78bd
mouMon25 <moumon1925@gmail.com>
2025-08-07 12:15:19 +0530

    index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import ShopContextProvider from './Context/ShopContext';
+import App from './App'; 
+import {ShopContextProvider} from './Context/ShopContext';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <ShopContextProvider>
+  <ShopContextProvider>
+    <HashRouter>
       <App />
-    </ShopContextProvider>
+    </HashRouter>
+  </ShopContextProvider>
 );
