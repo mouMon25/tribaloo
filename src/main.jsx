b08b3aca44fec13d3a4ecb51113eb9a1752cc538
mouMon25 <moumon1925@gmail.com>
2025-08-07 12:01:51 +0530

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-
+import ShopContextProvider from './Context/ShopContext.jsx';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ShopContextProvider>
+      <App />
+    </ShopContextProvider>
   </React.StrictMode>
 );
